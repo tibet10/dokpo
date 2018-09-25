e84222db521c7558e7e758f534ed17341e45c3c5
Tashi Topgyal <choezomtsering129@gmail.com>
2018-09-25 00:27:21 -0400

    configuring db name

--- dokpo/settings.py
+++ dokpo/settings.py
@@ -123,7 +123,7 @@ LOGIN_REDIRECT_URL = '/account/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
-DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
+#DATABASES['default'] = dj_database_url.config(default=config('DATABASE_URL'))
