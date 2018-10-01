3661dbef096d20807e6ec433f563fde835de88dd
Tashi Topgyal <choezomtsering129@gmail.com>
2018-10-01 19:34:52 -0400

    small changes

--- dokpo/settings.py
+++ dokpo/settings.py
@@ -123,7 +123,4 @@ LOGIN_REDIRECT_URL = '/account/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DATABASES['default'].update(db_from_env)
-
 DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
