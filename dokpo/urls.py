ec41f8ae4178fea8808c344d87c750dd973d00d1
Tashi Topgyal <choezomtsering129@gmail.com>
2018-11-18 15:35:41 -0500

    nav.html;

--- dokpo/urls.py
+++ dokpo/urls.py
@@ -5,11 +5,11 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     url(r'', include('core.urls', namespace='core')),
     url(r'^admin/', admin.site.urls),
-    url(r'^accounts/', include('accounts.urls', namespace='accounts')),
+    # url(r'^accounts/', include('accounts.urls', namespace='accounts')),
     url(r'^payments/', include('payments.urls', namespace='payments')),
     url(r'^groups/', include('groups.urls', namespace='groups')),
 
-    url(r'^fb/', include('allauth.urls')),
+    url(r'^accounts/', include('allauth.urls')),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
