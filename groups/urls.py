88cb4738124e2a88c0bdbdfb7f978e1131fe8c19
Tashi Topgyal <choezomtsering129@gmail.com>
2018-12-17 23:42:03 -0500

    admin base

--- groups/urls.py
+++ groups/urls.py
@@ -3,12 +3,6 @@ from . import views
 
 app_name = 'dokpo'
 
-# urlpatterns = [
-#     url(r'^$', views.home, name='home'),
-#     url(r'(?P<id>\d+)/$', views.group_detail, name='detail'),
-#     url(r'^create/$', views.group_create, name='create')
-# ]
-
 urlpatterns = [
     url(r'^$', views.GroupView.as_view(), name='home'),
     url(r'(?P<pk>\d+)/$', views.GroupDetail.as_view(), name='detail'),
